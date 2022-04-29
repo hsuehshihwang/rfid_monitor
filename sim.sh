ef0b17353dc1064fd04785f945eeee186c66c4ba
Ralph Wang <ralph@status.com.tw>
2022-04-29 11:27:55 +0800

    update sim.sh for 5 loops.

--- sim.sh
+++ sim.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 car1rfid="ID NO:8200000013A3000000000000;ANT NO:01;User Code:08"
 
-echo $car1rfid >> telnet
-sleep 0.8
-echo $car1rfid >> telnet
-sleep 0.8
-for i in $(seq 1 1 10); do
+# start 
+for i in $(seq 1 1 100); do
 #	date +"%T.%6N"
 	echo $car1rfid >> telnet
 	sleep 0.05
@@ -14,8 +11,135 @@ echo $car1rfid >> telnet
 sleep 0.6
 echo $car1rfid >> telnet
 sleep 0.8
+echo $car1rfid >> telnet
+sleep 1
+echo $car1rfid >> telnet
+sleep 2
+
+sleep 120 
+
+# loop 1 
+echo $car1rfid >> telnet
+sleep 2
+echo $car1rfid >> telnet
+sleep 1
+echo $car1rfid >> telnet
+sleep 0.8
+echo $car1rfid >> telnet
+sleep 0.6
+for i in $(seq 1 1 18); do
+	echo $car1rfid >> telnet
+	sleep 0.05
+done
+echo $car1rfid >> telnet
+sleep 0.6
+echo $car1rfid >> telnet
+sleep 0.8
+echo $car1rfid >> telnet
+sleep 1
+echo $car1rfid >> telnet
+sleep 2
+
+sleep 115
+
+# loop 2
+echo $car1rfid >> telnet
+sleep 2
+echo $car1rfid >> telnet
+sleep 1
+echo $car1rfid >> telnet
+sleep 0.8
+echo $car1rfid >> telnet
+sleep 0.6
+for i in $(seq 1 1 17); do
+	echo $car1rfid >> telnet
+	sleep 0.05
+done
+echo $car1rfid >> telnet
+sleep 0.6
+echo $car1rfid >> telnet
+sleep 0.8
+echo $car1rfid >> telnet
+sleep 1
+echo $car1rfid >> telnet
+sleep 2
+
+sleep 110
+
+# loop 3
+echo $car1rfid >> telnet
+sleep 2
+echo $car1rfid >> telnet
+sleep 1
+echo $car1rfid >> telnet
+sleep 0.8
+echo $car1rfid >> telnet
+sleep 0.6
+for i in $(seq 1 1 18); do
+	echo $car1rfid >> telnet
+	sleep 0.05
+done
+echo $car1rfid >> telnet
+sleep 0.6
+echo $car1rfid >> telnet
+sleep 0.8
+echo $car1rfid >> telnet
+sleep 1
+echo $car1rfid >> telnet
+sleep 2
+
+sleep 107
+
+# loop 4
+echo $car1rfid >> telnet
+sleep 2
+echo $car1rfid >> telnet
+sleep 1
+echo $car1rfid >> telnet
+sleep 0.8
+echo $car1rfid >> telnet
+sleep 0.6
+for i in $(seq 1 1 16); do
+	echo $car1rfid >> telnet
+	sleep 0.05
+done
+echo $car1rfid >> telnet
+sleep 0.6
+echo $car1rfid >> telnet
+sleep 0.8
+echo $car1rfid >> telnet
+sleep 1
+echo $car1rfid >> telnet
+sleep 2
+
+sleep 105
+
+# loop 5
+echo $car1rfid >> telnet
+sleep 2
+echo $car1rfid >> telnet
+sleep 1
+echo $car1rfid >> telnet
+sleep 0.8
+echo $car1rfid >> telnet
+sleep 0.6
+for i in $(seq 1 1 15); do
+	echo $car1rfid >> telnet
+	sleep 0.05
+done
+echo $car1rfid >> telnet
+sleep 0.6
+echo $car1rfid >> telnet
+sleep 0.8
+echo $car1rfid >> telnet
+sleep 1
+echo $car1rfid >> telnet
+sleep 2
+
+sleep 103
 
 exit; 
+# As below, it's the log provided from Nick. 
 ID NO:8200000013A3000000000000;ANT NO:01;User Code:08
 ID NO:8200000013A3000000000000;ANT NO:01;User Code:08
 ID NO:8200000013A3000000000000;ANT NO:01;User Code:08
